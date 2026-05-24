97f8da3b828d17b5a9ce9f097a77aab8fab4e746
Linden <65407488+thelindat@users.noreply.github.com>
2026-05-24 21:09:31 +1000

    refactor(common/locale): use typed wrapper

--- resource/common/locale.ts
+++ resource/common/locale.ts
@@ -1,11 +1,3 @@
-import { locale, type FlattenObjectKeys } from "@overextended/ox_lib";
+import { createLocales } from "@overextended/ox_lib";
 
-type Locales = FlattenObjectKeys<typeof import("../../locales/en.json")>;
-
-function Locale<T extends Locales>(str: T, ...args: any[]): string;
-function Locale<T extends string>(str: T, ...args: any[]): string | unknown;
-function Locale<T extends string>(str: T, ...args: any[]) {
-  return locale(str, ...args);
-}
-
-export default Locale;
+export default createLocales<typeof import("../../locales/en.json")>();
